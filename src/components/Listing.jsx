248f3731d472128644ae03cfca7f9f35ba9e3417
Artem <artemklinkov@yandex.ru>
2021-04-26 23:53:18 +0300

    listing

--- src/components/Listing.jsx
+++ src/components/Listing.jsx
@@ -2,11 +2,10 @@ import React from "react";
 import Item from "./Item";
 
 const Listing = ({ items }) => {
-  const getID = listing_id => { return listing_id};
   return (
     <div className="item-list">
       {items.map(item => (
-        <Item item={item} key={getID(item.listing_id)} />
+        <Item item={item} key={item.listing_id} />
       ))}
     </div>
   );
